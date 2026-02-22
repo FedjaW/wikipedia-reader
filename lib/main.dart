ee8b759e557a45f253bed99b999258578f59db49
Friedrich Wilms <aureliasodyssey@gmail.com>
2026-02-22 13:42:26 +0100

    flutter pub add http

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        appBar: AppBar(title: const Text("Wikipedia Flutter")),
+        body: Center(child: Text('Loading...')),
       ),
     );
   }
